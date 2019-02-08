7c9b40d135a4e03695235c6184a21da47da300a0
Sabri <sabri.sonmez@macaulay.cuny.edu>
2019-02-07 23:20:45 -0500

    add file

---
+++ XamarinIOsFolder/CALCULATORios/ViewController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+
+using UIKit;
+
+namespace CALCULATORios
+{
+
+    public partial class ViewController : UIViewController
+    {
+
+        double firstNum;
+        double secondNum;
+        String holder = "";
+        double result;
+        bool button1clicked = false;
+        bool button2clicked = false;
+        bool button3clicked = false;
+        bool button4clicked = false;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            #region Buttons 1-9:
+            btn1.TouchUpInside += (sender, e) =>
+            {
+                textField.Text = textField.Text + btn1.TitleLabel.Text;
+            };
+            btn2.TouchUpInside += (sender, e) =>
+            {
+                textField.Text = textField.Text + btn2.TitleLabel.Text;
+            };
+            btn3.TouchUpInside += (sender, e) =>
+            {
+                textField.Text = textField.Text + btn3.TitleLabel.Text;
+            };
+            btn4.TouchUpInside += (sender, e) =>
+            {
+                textField.Text = textField.Text + btn4.TitleLabel.Text;
+            };
+            btn5.TouchUpInside += (sender, e) =>
+            {
+                textField.Text = textField.Text + btn5.TitleLabel.Text;
+            };
+            btn6.TouchUpInside += (sender, e) =>
+            {
+                textField.Text = textField.Text + btn6.TitleLabel.Text;
+            };
+            btn7.TouchUpInside += (sender, e) =>
+            {
+                textField.Text = textField.Text + btn7.TitleLabel.Text;
+            };
+            btn8.TouchUpInside += (sender, e) =>
+            {
+                textField.Text = textField.Text + btn8.TitleLabel.Text;
+            };
+            btn9.TouchUpInside += (sender, e) =>
+            {
+                textField.Text = textField.Text + btn9.TitleLabel.Text;
+            };
+            btn0.TouchUpInside += (sender, e) =>
+            {
+                textField.Text = textField.Text + btn0.TitleLabel.Text;
+            };
+            #endregion
+
+            #region Operation Buttons:
+
+            btnAdd.TouchUpInside += (sender, e) =>
+            {
+                firstNum = Convert.ToDouble(textField.Text);
+                textField.Text = holder;
+                button1clicked = true;
+            };
+            btnSub.TouchUpInside += (sender, e) =>
+            {
+                firstNum = Convert.ToDouble(textField.Text);
+                textField.Text = holder;
+                button2clicked = true;
+            };
+
+            btnMul.TouchUpInside += (sender, e) =>
+            {
+                firstNum = Convert.ToDouble(textField.Text);
+                textField.Text = holder;
+                button3clicked = true;
+            };
+
+            btnDiv.TouchUpInside += (sender, e) =>
+            {
+                firstNum = Convert.ToDouble(textField.Text);
+                textField.Text = holder;
+                button4clicked = true;
+
+            };
+
+            btnEql.TouchUpInside += (sender, e) =>
+            {
+                if (button1clicked == true)
+                {
+                    Calculate(1);
+                }
+                if (button2clicked == true)
+                {
+                    Calculate(2);
+                }
+                if (button3clicked == true)
+                {
+                    Calculate(3);
+                }
+                if (button4clicked == true)
+                {
+                    Calculate(4);
+                }
+
+            };
+
+            btnDel.TouchUpInside += (sender, e) =>
+            {
+                textField.Text = holder;
+                firstNum = 0;
+
+            };
+        }
+
+        #endregion
+
+        #region Calculator Logic:
+
+        public void Calculate(int opType){
+           
+            switch (opType)
+            {
+                case 1:
+                    secondNum = Convert.ToDouble(textField.Text);
+                    result = firstNum + secondNum;
+                    textField.Text = result.ToString();
+                    button1clicked = false;
+                    break;
+                case 2:
+                    secondNum = Convert.ToDouble(textField.Text);
+                    result = firstNum - secondNum;
+                    textField.Text = result.ToString();
+                    button2clicked = false;
+                    break;
+                case 3:
+                    secondNum = Convert.ToDouble(textField.Text);
+                    result = firstNum * secondNum;
+                    textField.Text = result.ToString();
+                    button3clicked = false;
+                    break;
+                case 4:
+                    secondNum = Convert.ToDouble(textField.Text);
+                    result = firstNum / secondNum;
+                    textField.Text = result.ToString();
+
+                    if(secondNum == 0){
+
+                        var okAlertController = UIAlertController.Create("ERROR", "Cannot divide by Zero.", UIAlertControllerStyle.Alert);
+
+                        //Add Action
+                        okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+
+                        // Present Alert
+                        PresentViewController(okAlertController, true, null);
+
+                        textField.Text = "ERROR";
+
+                    }
+
+                    button4clicked = false;
+                    break;
+            }
+        }
+        #endregion
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ XamarinIOsFolder/CALCULATORios/ViewController.designer.cs
@@ -0,0 +1,181 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace CALCULATORios
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn0 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn3 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn4 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn5 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn6 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn7 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn8 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn9 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnAdd { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnDel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnDiv { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnDot { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnEql { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnMul { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnSub { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textField { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btn0 != null) {
+                btn0.Dispose ();
+                btn0 = null;
+            }
+
+            if (btn1 != null) {
+                btn1.Dispose ();
+                btn1 = null;
+            }
+
+            if (btn2 != null) {
+                btn2.Dispose ();
+                btn2 = null;
+            }
+
+            if (btn3 != null) {
+                btn3.Dispose ();
+                btn3 = null;
+            }
+
+            if (btn4 != null) {
+                btn4.Dispose ();
+                btn4 = null;
+            }
+
+            if (btn5 != null) {
+                btn5.Dispose ();
+                btn5 = null;
+            }
+
+            if (btn6 != null) {
+                btn6.Dispose ();
+                btn6 = null;
+            }
+
+            if (btn7 != null) {
+                btn7.Dispose ();
+                btn7 = null;
+            }
+
+            if (btn8 != null) {
+                btn8.Dispose ();
+                btn8 = null;
+            }
+
+            if (btn9 != null) {
+                btn9.Dispose ();
+                btn9 = null;
+            }
+
+            if (btnAdd != null) {
+                btnAdd.Dispose ();
+                btnAdd = null;
+            }
+
+            if (btnDel != null) {
+                btnDel.Dispose ();
+                btnDel = null;
+            }
+
+            if (btnDiv != null) {
+                btnDiv.Dispose ();
+                btnDiv = null;
+            }
+
+            if (btnDot != null) {
+                btnDot.Dispose ();
+                btnDot = null;
+            }
+
+            if (btnEql != null) {
+                btnEql.Dispose ();
+                btnEql = null;
+            }
+
+            if (btnMul != null) {
+                btnMul.Dispose ();
+                btnMul = null;
+            }
+
+            if (btnSub != null) {
+                btnSub.Dispose ();
+                btnSub = null;
+            }
+
+            if (textField != null) {
+                textField.Dispose ();
+                textField = null;
+            }
+        }
+    }
+}
